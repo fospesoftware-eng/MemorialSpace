737a8c9028c549f30ededcd2f2fa805ca2ec167b
fospesoftware-eng <fospesoftware@gmail.com>
2026-05-31 13:24:47 +0530

    Install map maker workflow enhancements

--- artifacts/memorial-space/src/main.tsx
+++ artifacts/memorial-space/src/main.tsx
@@ -3,7 +3,9 @@ import App from "./App";
 import "./index.css";
 import "./map-maker-preview.css";
 import { installMapMakerPublishSync } from "./lib/map-maker-publish-sync";
+import { installMapMakerWorkflowEnhancements } from "./lib/map-maker-workflow-enhancements";
 
 installMapMakerPublishSync();
+installMapMakerWorkflowEnhancements();
 
 createRoot(document.getElementById("root")!).render(<App />);
