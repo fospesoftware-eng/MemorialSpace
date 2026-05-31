594b5336926c47062fabedc30d45926995f5947b
fospesoftware-eng <fospesoftware@gmail.com>
2026-05-31 13:19:49 +0530

    Install published map sync hook

--- artifacts/memorial-space/src/main.tsx
+++ artifacts/memorial-space/src/main.tsx
@@ -2,5 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./map-maker-preview.css";
+import { installMapMakerPublishSync } from "./lib/map-maker-publish-sync";
+
+installMapMakerPublishSync();
 
 createRoot(document.getElementById("root")!).render(<App />);
