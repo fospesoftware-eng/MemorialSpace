fac5cb87f5018c2f0e87501634cdc3adf12dbf9d
fospesoftware-eng <fospesoftware@gmail.com>
2026-05-31 13:11:35 +0530

    Load map maker preview sidebar styles

--- artifacts/memorial-space/src/main.tsx
+++ artifacts/memorial-space/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import "./map-maker-preview.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
